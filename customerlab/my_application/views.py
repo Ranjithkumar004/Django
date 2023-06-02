265077a32d0e4f2ea7be4fca700a4f4b0fb43b88
Ranjith <113409709+Ranjithkumar004@users.noreply.github.com>
2023-06-02 12:43:33 +0530

    Add files via upload

---
+++ customerlab/my_application/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request,"home.html")
+
+def register(request):
+    name=request.POST['name']
+    password=request.POST['password']
+    address=request.POST['address']
+    mail=request.POST['mail']
+    return render(request,"output.html",{'name':name,'password':password,'address':address,'mail':mail})
